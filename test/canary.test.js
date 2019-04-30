256a6f2df8def657cbe2bddb0b35f243e1cf8a7e
Charles <charlesmbrady@gmail.com>
2019-04-30 01:00:51 -0400

    need to revisit lint rules

--- test/canary.test.js
+++ test/canary.test.js
@@ -1,7 +1,7 @@
 var expect = require("chai").expect;
 
-describe("Multiply", function() {
-  it("should return true", function() {
+describe("Multiply", function () {
+  it("should return true", function () {
     expect(true).to.equal(true);
   });
 });
