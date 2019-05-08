7e80bb7d29ec77107d77946576721070db0ffa8b
Desmond Mullen <dsm@busylittlestudios.com>
2019-05-08 14:39:47 -0400

    buttons function

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -6,26 +6,26 @@ import './menu.css';
 const MenuModal = (props) => {
     return (
         <div>
-            <Button color="secondary" onClick={props.toggle} className="menu-btn" data-test="home-menu-button">. . .</Button>
-            <Modal isOpen={props.modal} toggle={props.toggle} className="menu">
-                <ModalHeader toggle={props.toggle}>
+            <Button color="secondary" onClick={ props.toggle } className="menu-btn" data-test="home-menu-button">. . .</Button>
+            <Modal isOpen={ props.modal } toggle={ props.toggle } className="menu">
+                <ModalHeader toggle={ props.toggle }>
                     <Container>
                         <Row>
                             <Col>
-                            {props.user ? (<h2>{props.user.firstName}</h2>) : 
-                            <Link to='/login'>Login</Link>
-                        }
+                                { props.user ? (<h2>{ props.user.firstName }</h2>) :
+                                    <Link to='/login'>Login</Link>
+                                }
                             </Col>
                         </Row>
                         <Row>
                             <Col>
-                                {!props.user ? <Link to='/signup'>Signup</Link> : null}
+                                { !props.user ? <Link to='/signup'>Signup</Link> : null }
                             </Col>
                         </Row>
-                        
+
                         <Row>
                             <Col>
-                            {props.user ? (<Link to="#" className="logout" onClick={props.logout}>Logout</Link>) : null}
+                                { props.user ? (<Link to="#" className="logout" onClick={ props.logout }>Logout</Link>) : null }
                             </Col>
                         </Row>
                     </Container>
@@ -34,12 +34,12 @@ const MenuModal = (props) => {
                     <Container>
                         <Row>
                             <Col>
-                                <Link to='/history' data-test="menu-history">History</Link>
+                                <Link to='/alerts' data-test="menu-alerts">Alerts</Link>
                             </Col>
                         </Row>
                         <Row>
                             <Col>
-                                <Link to='/friends' data-test="menu-friends">Friends</Link>
+                                <Link to='/history' data-test="menu-history">History</Link>
                             </Col>
                         </Row>
                         <Row>
@@ -47,20 +47,15 @@ const MenuModal = (props) => {
                                 <Link to='/settings' data-test="menu-settings">Settings</Link>
                             </Col>
                         </Row>
-                        <Row>
-                            <Col>
-                                <Link to='/rideshare' data-test="menu-ride">Get a Ride</Link>
-                            </Col>
-                        </Row>
                     </Container>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="secondary" onClick={props.toggle}>Cancel</Button>
+                    <Button color="secondary" onClick={ props.toggle }>Close</Button>
                 </ModalFooter>
             </Modal>
         </div>
     )
-    
+
 };
 
 export default MenuModal;
