9d911dc1193e88de0c8b867d038a6777ce2b0342
Charles <charlesmbrady@gmail.com>
2019-05-11 13:13:06 -0400

    made menu buttons open modals for settings and alerts

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -2,30 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './menu.css';
+import AlertsModal from '../AlertsModal/AlertsModal';
 
 const MenuModal = (props) => {
     return (
         <div>
-            <Button color="secondary" onClick={ props.toggle } className="menu-btn" data-test="home-menu-button">Menu</Button>
-            <Modal isOpen={ props.modal } toggle={ props.toggle } className="menu">
-                <ModalHeader toggle={ props.toggle }>
+            <Button color="secondary" onClick={props.toggle} className="menu-btn" data-test="home-menu-button">Menu</Button>
+            <Modal isOpen={props.modal} toggle={props.toggle} className="menu">
+                <ModalHeader toggle={props.toggle}>
                     <Container>
                         <Row>
                             <Col className="menu-button">
-                                { props.user ? (<h2>{ props.user.firstName }</h2>) :
+                                {props.user ? (<h2>{props.user.firstName}</h2>) :
                                     <Link to='/login'>Login</Link>
                                 }
                             </Col>
                         </Row>
                         <Row>
                             <Col className="menu-button">
-                                { !props.user ? <Link to='/signup'>Signup</Link> : null }
+                                {!props.user ? <Link to='/signup'>Signup</Link> : null}
                             </Col>
                         </Row>
 
                         <Row>
                             <Col className="menu-button">
-                                { props.user ? (<Link to="#" className="logout" onClick={ props.logout }>Logout</Link>) : null }
+                                {props.user ? (<Link to="#" className="logout" onClick={props.logout}>Logout</Link>) : null}
                             </Col>
                         </Row>
                     </Container>
@@ -39,7 +40,7 @@ const MenuModal = (props) => {
                         </Row>
                         <Row>
                             <Col className="menu-button">
-                                <Link to='/alerts' data-test="menu-alerts">Alerts</Link>
+                                <button data-test="menu-alerts" toggleAlerts={props.toggleAlerts} onClick={props.toggleAlerts}>Alerts</button>
                             </Col>
                         </Row>
                         <Row>
@@ -49,7 +50,7 @@ const MenuModal = (props) => {
                         </Row>
                         <Row>
                             <Col className="menu-button">
-                                <Link className="menu-link" to='/settings' data-test="menu-settings">Settings</Link>
+                            <button data-test="menu-settings" toggleSettings={props.toggleSettings} onClick={props.toggleSettings}>Settings</button>
                             </Col>
                         </Row>
                     </Container>
