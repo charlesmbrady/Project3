f24232800fc53c8284c193ac2adf0761f52dd6ec
Charles <charlesmbrady@gmail.com>
2019-05-10 23:52:58 -0400

    added home button

--- client/src/pages/Quickstart.js
+++ client/src/pages/Quickstart.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import HomeButton from '../components/HomeButton/HomeButton';
 
-function Quickstart () {
+function Quickstart() {
 
     return (
-        <>
+        <div>
+            <HomeButton />
             <p id="how-to">Click <em>Check-in</em> to keep track of where your stuff is (your credit card on a bar tab, your jacket, your friends). Click <em>Add Drink to Count</em> to keep track of your drinks over time to get a rough estimate of your blood-alcohol level. Click <em>Contact Friends</em> to send a link to your location to friends. Click <em>Get an Uber</em> to get a safe ride home.</p>
-        </>
-    );
-}
+        </div>
+    )
+};
 
 
 export default Quickstart;
