660c2265001792e094fdb886c1b0de969a8d2aac
Charles <charlesmbrady@gmail.com>
2019-05-11 11:44:45 -0400

    added alerts and settings page and restyled login/register

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './style.css';
 
 export const Card = (props) => (
   <div className="card">
-    <div className="card-header bg-primary" style={{color: '#fff'}}>
+    <div className="card-header">
       <h5>{props.title}</h5>
     </div>
     <div className="card-body">
