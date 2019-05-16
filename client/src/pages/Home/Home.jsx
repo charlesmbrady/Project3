949f1f4c83749fa301f6d493c3fcc052876c24cf
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2019-05-16 10:49:52 -0400

    link time to zero bac to current bac countdown

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -63,7 +63,7 @@ class Home extends Component {
       let now = new Date();
       let elapsedTime = (now - new Date(lastdrink.timeOfLastDrink)) / 60000;
       let bac = (res.data.drinks[ (res.data.drinks.length) - 1 ].bac - (elapsedTime * .00025)).toFixed(5);
-      if (bac < 0) { bac = 0; zero=0;}
+      if (bac < 0) { bac = 0; }
       //measure the time based on current bac for it to get to 0
       let counter = 0, baczero = bac;
       while (baczero > 0) {
@@ -71,6 +71,8 @@ class Home extends Component {
         counter++;
       }
       let zero = (counter / 60).toFixed(2);
+
+      if (bac < 0) { zero=0;}
 
       //Begin calculate history summary based on date
       let dateArr=[];
@@ -136,7 +138,7 @@ class Home extends Component {
   //update BAC every minute
   updateBac () {
     let bac = (this.state.bac - ((1 / 60) * .015)).toFixed(5);
-    if (bac < 0) { bac = 0; zero=0;}
+    if (bac < 0) { bac = 0; }
     //measure the time based on current bac for it to get to 0
     let counter = 0, baczero = bac;
     while (baczero > 0) {
@@ -144,6 +146,7 @@ class Home extends Component {
       counter++;
     }
     let zero = (counter / 60).toFixed(2);
+    if (bac < 0) { zero=0;}
     this.setState({ bac, zero });
   }
 
@@ -159,7 +162,7 @@ class Home extends Component {
       numberOfDrinksCopy.push(lastdrink);
       let bac = (parseFloat(this.calculateBac(1, lastdrink.timeOfLastDrink, this.state.weight, this.state.gender)) +
         parseFloat(this.state.bac)).toFixed(5);
-      if (bac < 0) { bac = 0; zero=0;}
+      if (bac < 0) { bac = 0; }
       //measure the time based on current bac for it to get to 0
       let counter = 0, baczero = bac;
       while (baczero > 0) {
@@ -167,6 +170,7 @@ class Home extends Component {
         counter++;
       }
       let zero = (counter / 60).toFixed(2);
+      if (bac < 0) { zero=0; }
       this.setState({ numberOfDrinks: numberOfDrinksCopy, bac, zero },
         () => this.checkBeforeSendAutomaticText());
       //push drinks to db
